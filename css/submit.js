70df19d86e0d82ad0ec55be0f66c0167f0111a38
BeccaStaple <56489791+BeccaStaple@users.noreply.github.com>
2019-11-19 22:08:46 +0000

    Attempt at a form
    Got started on the "your workout" page

---
+++ css/submit.js
@@ -0,0 +1,13 @@
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function() {
+    realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+if (realFileBtn.value) {
+    customTxt.innerHTML = "No file chosen yet";
+}
+});
