22457c719e45578730742013a0ba8e25db3a8bf9
BeccaStaple <56489791+BeccaStaple@users.noreply.github.com>
2019-11-23 12:47:07 +0000

    redesigned homepage
    Completely different look!

--- css/submit.js
+++ css/submit.js
@@ -1,13 +1,20 @@
-const realFileBtn = document.getElementById("real-file");
+ 
 const customBtn = document.getElementById("custom-button");
 const customTxt = document.getElementById("custom-text");
+// console.log(realFileBtn);
 
-customBtn.addEventListener("click", function() {
-    realFileBtn.click();
-});
+// customBtn.addEventListener("click", function() {
+//     realFileBtn.click();
+// });
 
-realFileBtn.addEventListener("change", function() {
-if (realFileBtn.value) {
-    customTxt.innerHTML = "No file chosen yet";
+// realFileBtn.addEventListener("change", function() {
+// if (realFileBtn.value) {
+//     customTxt.innerHTML = "No file chosen yet";
+// }
+// });
+function chooseFile() {
+    const realFileBtn = document.getElementById("real-file");
+    console.log("that thing i wanted :", realFileBtn);
+    realFileBtn.click()
+
 }
-});
