0eb0c09096e38d1a9c48a14bc6a56a2a15dc8443
katrinaada <56648717+katrinaada@users.noreply.github.com>
2019-11-30 23:34:40 +0000

    added unstylised sidenav to sign up page

--- sign-up.js
+++ sign-up.js
@@ -5,3 +5,12 @@ const container = document.getElementById("container");
 signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
 
 signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
+
+function opensidenav() {
+    document.getElementById('sidenav').style.left="0px";
+    document.getElementById('openbutton').style.display="none";
+}
+function closesidenav() {
+    document.getElementById('sidenav').style.left="-200px";
+    document.getElementById('openbutton').style.display="block";
+}
