b7ec69f13213af5c80dc8fa59b3fe856a9d256a1
katrinaada <56648717+katrinaada@users.noreply.github.com>
2019-11-30 16:08:20 +0000

    unstylised sidenav
    just added this to the main page for now so you can see but havent stylised it (e.g. colour, size, font of navbar and menu button)

---
+++ main.js
@@ -0,0 +1,8 @@
+function opensidenav() {
+    document.getElementById('sidenav').style.left="0px";
+    document.getElementById('openbutton').style.display="none";
+}
+function closesidenav() {
+    document.getElementById('sidenav').style.left="-200px";
+    document.getElementById('openbutton').style.display="block";
+}
