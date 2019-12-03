ad10533499d16d82366e968d188a66e947ab75d0
BeccaStaple <56489791+BeccaStaple@users.noreply.github.com>
2019-12-03 18:58:46 +0000

    added body link to navbar

--- body.js
+++ body.js
@@ -10,10 +10,10 @@
         
     }
    
-    function closeHead() {
-        var headModalElement = document.getElementById ("headModalId")
-        headModalElement.style.display = "none"
-    }
+    //function closeHead() {
+        //var headModalElement = document.getElementById ("headModalId")
+        //headModalElement.style.display = "none"
+    //}
     
     //Arms click & close
     function openArms() {
@@ -23,10 +23,10 @@
         document.getElementById("absModalId").style.display = "none"
         document.getElementById("armsModalId").style.display = "block"
     }
-    function closeArms() {
-        var armsModalElement = document.getElementById ("armsModalId")
-        armsModalElement.style.display = "none"
-    }
+    //function closeArms() {
+        //var armsModalElement = document.getElementById ("armsModalId")
+        //armsModalElement.style.display = "none"
+    //}
 
     //Abs click & close
     function openAbs() {
@@ -36,10 +36,11 @@
         document.getElementById("legsModalId").style.display = "none"
         document.getElementById("absModalId").style.display = "block"
     }
-    function closeAbs() {
-        var absModalElement = document.getElementById ("absModalId")
-        absModalElement.style.display = "none"
-    }
+    //function closeAbs() {
+        //var absModalElement = document.getElementById ("absModalId")
+        //absModalElement.style.display = "none"
+    //}
+
     //legs click & close
     function openLegs() {
         document.getElementById("starter").style.display = "none"
@@ -48,8 +49,8 @@
         document.getElementById("absModalId").style.display = "none"
         document.getElementById("legsModalId").style.display = "block"
     }
-    function closeLegs() {
-        var legsModalElement = document.getElementById ("legsModalId")
-        legsModalElement.style.display = "none"
-    }
+    //function closeLegs() {
+        //var legsModalElement = document.getElementById ("legsModalId")
+        //legsModalElement.style.display = "none"
+    //}
 
